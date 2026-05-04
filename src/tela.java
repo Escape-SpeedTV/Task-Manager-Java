2f2b9d1f8003c394f1db8ed62d80e7d071b3c3a0
William Nakano <escapspeed1@gmail.com>
2026-05-04 06:58:42 -0300

    Mudança na parte visual do usuário

--- src/tela.java
+++ src/tela.java
@@ -19,10 +19,10 @@ public class tela {
 
         JPanel painelTopo = new JPanel(new FlowLayout());
         JTextField campo = new JTextField(20);
-        JButton BotaoAdicionar = new JButton("Adicionar");
+        JButton botaoAdicionar = new JButton("Adicionar");
         painelTopo.add(new JLabel("Tarefa"));
         painelTopo.add(campo);
-        painelTopo.add(BotaoAdicionar);
+        painelTopo.add(botaoAdicionar);
         frame.add(painelTopo, BorderLayout.NORTH);
 
         //Painel Central
@@ -48,5 +48,61 @@ public class tela {
         frame.add(painelBaixo, BorderLayout.SOUTH);
 
         //Painel Action
+        botaoAdicionar.addActionListener(e ->{
+            String texto = campo.getText();
+            if (!texto.trim().isEmpty()){
+                gerenciador.adicionarTarefa(texto);
+                gerenciador.salvarTarefas();
+                atualizarArea(area, gerenciador);
+                campo.setText("");
+            }else {
+                JOptionPane.showMessageDialog(frame, "Digite uma tarefa!");
+            }
+        });
+
+        botaoConcluir.addActionListener(e ->{
+            try{
+                int index = Integer.parseInt(campoIndice.getText());
+                gerenciador.concluirTarefa(index);
+                gerenciador.salvarTarefas();
+                atualizarArea(area, gerenciador);
+            }catch (Exception ex){
+                JOptionPane.showMessageDialog(frame, "Indice Inválido");
+            }
+        });
+
+        botaoRemover.addActionListener(e -> {
+            try {
+                int index = Integer.parseInt(campoIndice.getText());
+                gerenciador.removerTarefa(index);
+                gerenciador.salvarTarefas();
+                atualizarArea(area, gerenciador);
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(frame, "Índice inválido!");
+            }
+        });
+
+        botaoDesmarcar.addActionListener(e -> {
+            try {
+                int index = Integer.parseInt(campoIndice.getText());
+                gerenciador.desmarcarT(index);
+                gerenciador.salvarTarefas();
+                atualizarArea(area, gerenciador);
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(frame, "Índice inválido!");
+            }
+        });
+
+        //Atualizar a tela
+
+        atualizarArea(area, gerenciador);
+        frame.setVisible(true);
+    }
+
+    public static void atualizarArea(JTextArea area, gerenciarTarefas gerenciador){
+        area.setText("");
+        for (int i = 0; i < gerenciador.tarefas.size(); i++){
+            area.append(i + " - " + gerenciador.tarefas.get(i) + "\n");
+        }
     }
 }
